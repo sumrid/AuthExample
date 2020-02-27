6ae1c56c0fde3b62d3aa378b218802b70ed66195
Sumrid Klinkanha <59070174@it.kmitl.ac.th>
2020-02-27 20:57:11 +0700

    [update] เพิ่ม refresh token

--- src/router.ts
+++ src/router.ts
@@ -6,11 +6,16 @@ import auth from './auth';
 
 const router = Router();
 const SECRET = "myKey";
+const REFRESH_SECRET = "refreshTokenSecert";
+const refreshTokens: any = {};
 
 passport.use(auth.passportJwtAuth()); // เรียก jwt strategy ที่ทำไว้
 
 router.post('/login', login);
+router.post('/token', issueToken);
+router.get('/tokens', getTokens);
 router.get('/basic', basicAuth({ authorizer: auth.basic }), hello);
+
 router.get('/jwt', auth.jwtAuth, hello);
 router.get('/jwt/passport', passport.authenticate("jwt", { session: false }), hello); // auth with passport.js
 
@@ -31,10 +36,37 @@ function login(req: Request, res: Response) {
         sub: username
     }
 
-    // TODO : generate refresh token
+    // ออก token
+    const token = jwt.sign(payload, SECRET, { expiresIn: 60 });
+    const refreshToken = jwt.sign(payload, REFRESH_SECRET, { expiresIn: "15d" });
+    refreshTokens[refreshToken] = username;
 
-    const token = jwt.sign(payload, SECRET, { expiresIn: 60 }); // ออก token
+    res.json({ jwt: token, refreshToken });
+}
+
+/**
+ * ขอ access token ใหม่
+ * @param req 
+ * @param res 
+ */
+function issueToken(req: Request, res: Response) {
+    const refreshToken = req.body.refreshToken;
+    const username = req.body.username;
+
+    // check refresh token
+    if (!(refreshToken in refreshTokens)) {
+        return res.status(401).send("token invalid.");
+    }
+
+    const payload = {
+        sub: username
+    }
+    const token = jwt.sign(payload, SECRET, { expiresIn: 60 });
     res.json({ jwt: token });
+}
+
+function getTokens(req: Request, res: Response) {
+    res.json(refreshTokens);
 }
 
 module.exports = router;
